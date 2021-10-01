73eb6ec0e87d116692e8ac9eaac0a77b19902b9f
Jacob Dennis P <jacoden143@gmail.com>
2021-10-01 12:28:03 +0530

    Updated Dynamic Insert

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Examples of GET and POST</title>
+    </head>
+    <body>
+        <form method="POST" action="MysqlDynamicDataInsert.php">
+            <input type="text" name="name" placeholder="Enter your Name"><br>
+            <input type="text" name="email" placeholder="Enter your email"><br>
+            <input type="password" name="password" placeholder="Enter your password"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
