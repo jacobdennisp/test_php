b6c6d153588185f74deb5f7e2bbfc0c81120a0f5
Jacob Dennis P <jacoden143@gmail.com>
2021-10-01 12:18:17 +0530

    Added code to insert data to table

---
+++ MysqlStaticDataInsert.php
@@ -0,0 +1,28 @@
+<?php
+    $connection = mysqli_connect("127.0.0.1","root","","php_test_database");
+
+    //print_r($connection);
+
+    if($connection === false){
+        die("Something Went Wrong with your connection : For more information check Error Stack<br>".mysqli_connect_error());
+    }
+
+    echo "Connection Created Successfully<br>";
+    echo "Connection Host info is as follows <br>".mysqli_get_host_info($connection);
+
+    $name='Jacob';
+    $email='jacoden153@gmail.com';
+    $password='Asdf1234';
+    $created_at=1;
+    
+    $query = "INSERT INTO users(name,email,password,created_by)VALUE('$name','$email','$password','$created_at')";
+
+    if(mysqli_query($connection,$query)){
+        echo "<br>Data has been inserted successfully!";
+    }else{
+        echo "<br>Data could not be inserted";
+        echo "<br>Check Error Trace<br>".mysqli_error($connection);
+    }
+
+    mysqli_close($connection);
+?>
