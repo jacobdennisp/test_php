81ec5563c677bd791abbf2d4b7f98ec8ff50d37a
Jacob Dennis P <jacoden143@gmail.com>
2021-10-01 11:17:00 +0530

    Added Files

---
+++ formfetch.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <p>your first name is : <?php echo$_POST['first_name']; ?> </p>
+        <p>your last name is : <?php echo$_POST['last_name']; ?> </p>
+    </body>
+</html>
+
+
+
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Examples of GET and POST</title>
+    </head>
+    <body>
+        <form method="POST" action="formfetch.php">
+            <input type="text" name="first_name" placeholder="First Name">
+            <input type="text" name="last_name" placeholder="Last name">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
